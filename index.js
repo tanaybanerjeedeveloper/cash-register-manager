af4a6898c41de60ebc9433c56640d36170205ebc
tanay_123 <tanay@tortalabz.com>
2021-08-23 20:09:40 +0530

    feat: added the feature of calculating the no of notes and some error statements

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const billAmount = document.querySelector('#bill-amount');
 const cashGiven = document.querySelector('#cash-given');
 const btn = document.querySelector('.btn');
 const errorMessage = document.querySelector('.error-message');
+const noOfNotes = document.querySelectorAll('.no-of-notes');
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 // btn.addEventListener('click', () => {
 //   console.log('clicked');
@@ -28,6 +31,14 @@ btn.addEventListener('click', () => {
   } else {
     console.log('its not Nan');
     if (billAmountConverted > 0) {
+      if (Number(cashGiven.value) >= Number(billAmount.value)) {
+        console.log(cashGiven.value);
+        console.log(billAmount.value);
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        calculateNoOfNotes(amountToBeReturned);
+      } else {
+        showError('Cash given must be greater or equal to the Bill Amount');
+      }
     } else {
       showError('Bill Amount must be greater than 0');
     }
@@ -39,4 +50,12 @@ const showError = (msg) => {
   errorMessage.style.color = 'red';
   errorMessage.style.marginBottom = '2rem';
   errorMessage.innerText = msg;
+};
+
+const calculateNoOfNotes = (amountToBeReturned) => {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 };
